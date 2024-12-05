61c15f5a4fe7db3a97335a6a5332dedf81ad1624
JerryWski <jarek_wasilewski@yahoo.com>
2024-12-06 00:19:50 +0100

    updating v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(),icon()]
+  integrations: [react(), tailwind(), icon()],
 });
